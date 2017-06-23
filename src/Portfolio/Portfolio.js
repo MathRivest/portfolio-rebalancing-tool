47a6161d8e0697d075014dd2f48dc7c383e7dd65
Mathieu Rivest <rivest.mathieu@gmail.com>
2017-06-23 13:47:27 -0400

    fix rebalance method

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -121,7 +121,7 @@ class Portfolio extends React.Component {
     }
 
     handleBalancePortfolioButtonClick = () => {
-        let balancedSecurities = SecurityHelpers.getBalanceList(this.state.securities, this.state.cash);
+        let balancedSecurities = SecurityHelpers.getBalancedList(this.state.securities, this.state.cash);
         this.setState((prevState, props) => PortfolioHelpers.updateSecurities(prevState, balancedSecurities));
     }
 
--- src/Portfolio/Security/SecurityHelpers.js
+++ src/Portfolio/Security/SecurityHelpers.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-let sumList = (arr, start) => {
+const sumList = (arr, start) => {
     let acc = start || 0;
     return _.reduce(arr, (sum, val) => {
         if(_.isNumber(val)) {
@@ -11,53 +11,67 @@ let sumList = (arr, start) => {
     }, acc);
 };
 
-let multiplyValues = (multiplier, multiplicand) => {
+const multiplyValues = (multiplier, multiplicand) => {
     return _.multiply(multiplier, multiplicand)
 }
 
-let getTotalWithCash = (securities, cash, property) => {
+const getTotalWithCash = (securities, cash, property) => {
     return sumList(_.map(securities, property), cash[property]);
 }
 
-let getPercentOf = (val, total) => {
+const getPercentOf = (val, total) => {
     let result = _.round((val / total) * 100, 2);
     result = _.isNaN(result) ? 0 : result;
     return result;
 }
 
-let getTotalofMultiplied = (securities, propertyA, propertyB) => {
+const getTotalofMultiplied = (securities, propertyA, propertyB) => {
     return _.reduce(securities, (sum, val) => {
         return sum + multiplyValues(val[propertyA], val[propertyB]);
     }, 0);
 }
 
-let getBalanceList = (securities, cash) => {
-    let total = getTotalWithCash(securities, cash, 'mktValue');
-    let moneyLeft = cash.mktValue;
-    // let balancedList = _.map(securities, (security) => {
-    //     let amountInCashNeeded = ((total * security.portPercentTarget) / 100) - security.mktValue,
-    //         amountInUnitNeeded = Math.floor(amountInCashNeeded / security.cost);
-    //     if(amountInUnitNeeded > 0) {
-    //         moneyLeft = moneyLeft - (amountInUnitNeeded * security.cost);
-    //         security.buyQty = amountInUnitNeeded;
-    //     } else {
-    //         security.buyQty = 0;
-    //     }
-    //     return security
-    // });
-    // return _.map(securities, (security) => {
-    //     let price = multiplyValues(security.cost, security.buyQty);
-    //     let newPercent = getPercentOf(security.mktValue + price, total);
-
-    //     let lowerThanTarget = newPercent < security.portPercentTarget;
-    //     let enoughToBuy = moneyLeft > 0 && moneyLeft > security.cost;
-    //     console.log(lowerThanTarget, enoughToBuy);
-    //     while (enoughToBuy && lowerThanTarget) {
-    //         moneyLeft = moneyLeft - security.cost;
-    //         security.buyQty = security.buyQty + 1;
-    //     }
+const balancedWithoutSell = (securities, total, moneyLeft) => {
+    return _.map(securities, (security) => {
+        security.buyQty = 0;
+        let price = multiplyValues(security.cost, security.buyQty);
+        let newPercent = getPercentOf(security.mktValue + price, total);
+        while ((newPercent < security.portPercentTarget) && (moneyLeft > security.cost)) {
+            moneyLeft = moneyLeft - security.cost;
+            security.buyQty = security.buyQty + 1;
+            price = multiplyValues(security.cost, security.buyQty);
+            newPercent = getPercentOf(security.mktValue + price, total);
+        }
         return security
     });
 }
 
-export { multiplyValues, getTotalWithCash, getPercentOf, getTotalofMultiplied, getBalanceList };
+const balancedWithSell = (securities, total, moneyLeft) => {
+    const balancedList = _.map(securities, (security) => {
+        let amountInCashNeeded = ((total * security.portPercentTarget) / 100) - security.mktValue,
+            amountInUnitNeeded = Math.floor(amountInCashNeeded / security.cost);
+        moneyLeft = moneyLeft - (amountInUnitNeeded * security.cost);
+        security.buyQty = amountInUnitNeeded;
+        return security
+    });
+    return _.map(balancedList, (security) => {
+        while(moneyLeft && moneyLeft >= security.cost) {
+            moneyLeft = moneyLeft - security.cost
+            security.buyQty++
+        }
+    });
+}
+
+let getBalancedList = (securities, cash) => {
+    const wantToSell = false;
+    const total = getTotalWithCash(securities, cash, 'mktValue');
+    let moneyLeft = cash.mktValue;
+
+    if(wantToSell) {
+        return balancedWithSell(securities, total, moneyLeft);
+    } else {
+        return balancedWithoutSell(securities, total, moneyLeft);
+    }
+}
+
+export { multiplyValues, getTotalWithCash, getPercentOf, getTotalofMultiplied, getBalancedList };
